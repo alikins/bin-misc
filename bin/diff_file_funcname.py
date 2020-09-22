5ac34d23e2d62352f9e1fc959f2f22a86e5df797
Adrian Likins <alikins@redhat.com>
2020-09-21 20:14:14 -0400

    just most recent versions

--- bin/diff_file_funcname.py
+++ bin/diff_file_funcname.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from collections import defaultdict
 import fileinput
 import re
 
@@ -30,10 +31,23 @@ def process(line, filename):
 
 def main():
     filename = 'N/A'
+    file_methods_map = defaultdict(list)
     for line in fileinput.input():
         filename, method_name = process(line, filename)
-        if method_name:
-            print('%s: %s' % (filename, method_name))
+        # if method_name:
+        # print('%s: %s' % (filename, method_name or ''))
+        # file_methods_map[(filename, method_name)] = None
+        file_methods_map[filename].append(method_name)
+        #if method_name is None or filename not in file_methods_map:
+        #    file_methods_map[filename].append(method_name)
+
+    import pprint
+    pprint.pprint(file_methods_map)
+
+    for filename in sorted(file_methods_map):
+        methods = [x for x in file_methods_map[filename] if x is not None] or [None]
+        for meth in methods:
+            print('%s: %s' % (filename, meth or ''))
 
 
 if __name__ == "__main__":
--- bin/unescape.py
+++ bin/unescape.py
@@ -6,14 +6,17 @@ import fileinput
 
 def process(line):
     clean_line, _ = codecs.escape_decode(line)
-    #clean_line2 = codecs.escape_decode(clean_line)
-    #print clean_line2
-    return clean_line
+    clean_line2 = codecs.escape_decode(clean_line)
+    # return(clean_line2)
+    #print(clean_line2)
+    return(clean_line.decode('utf-8'))
+
 
 def main():
     for line in fileinput.input():
         clean_line = process(line)
-        print clean_line
+        print(clean_line)
+
 
 if __name__ == "__main__":
     main()
